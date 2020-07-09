adea77ce839d40cb807d0ffabc95e8788bf68f4d
Leon Montealegre <leonm99@gmail.com>
2020-07-09 12:22:09 -0400

    Improved isRef and isWrapper functions to work for all cases

--- src/Types.ts
+++ src/Types.ts
@@ -9,9 +9,9 @@ export type Wrapper = {type: string, data: DataType};
 export type RootType = Record<string, DataType>;
 
 export function isRef(dataType: DataType): dataType is Ref {
-    return "ref" in dataType;
+    return dataType && (dataType instanceof Object) && "ref" in dataType;
 }
 export function isWrapper(dataType: DataType): dataType is Wrapper {
-    return "type" in dataType && "data" in dataType;
+    return dataType && (dataType instanceof Object) && ("type" in dataType && "data" in dataType);
 }
 /*****************************************/
